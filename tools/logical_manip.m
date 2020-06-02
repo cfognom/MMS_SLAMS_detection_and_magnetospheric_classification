5ecdaac846a3a1e47b360e1d0086c95dae533866
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2020-06-02 15:37:15 +0200

    add logical_manip

---
+++ tools/logical_manip.m
@@ -0,0 +1,24 @@
+function logic = logical_manip(logic, method)
+%LOGICAL_MANIP Summary of this function goes here
+%   Detailed explanation goes here
+    logical(logic)
+    if nargin == 1
+        method = 'firstOfSimilar';
+    end
+    dif = diff(logic);
+    switch method
+        case 'firstOfSimilar'
+            logic = [true, dif ~= 0];
+        case 'firstOfOne'
+            logic = [logic(1) == 1, dif == 1];
+        case 'firstOfZero'
+            logic = [logic(1) == 0, dif == -1];
+        case 'lastOfSimilar'
+            logic = [dif ~= 0, true];
+        case 'lastOfOne'
+            logic = [dif == -1, logic(end) == 1];
+        case 'lastOfZero'
+            logic = [dif == 1, logic(end) == 0];
+    end
+end
+
