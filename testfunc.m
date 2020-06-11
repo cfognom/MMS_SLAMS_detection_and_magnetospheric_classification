31ad2d77c7045af7d9dcdb36993b84b3b7c51ba2
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2020-06-11 07:31:49 +0200

    new testFunc

--- testfunc.m
+++ testfunc.m
@@ -1,24 +1,15 @@
-function tints_SLAMS = testfunc(tints_SLAMS, tint_allowed)
+function testfunc()
 %TESTFUNC Summary of this function goes here
 %   Detailed explanation goes here
-    % if isempty(tints_SLAMS_SW)
-    %     tints_SLAMS = [];
-    %     return
-    % end
-    % mem = ismember(tints_SLAMS_SW.epoch, tints_SLAMS.epoch);
-    % dif = diff(mem);
-    % idx = dif(1:2:end) == 0;
-    % idx = reshape([idx, idx]', [], 1);
-    % tints_SLAMS = tints_SLAMS_SW(idx);
 
-    start_idx = find(tints_SLAMS >= tint_allowed(1), 1);
-    if mod(start_idx, 2) == 0
-        start_idx = start_idx + 1;
-    end
-    stop_idx = find(tints_SLAMS <= tint_allowed(2), 1, 'last');
-    if mod(stop_idx, 2) == 1
-        stop_idx = stop_idx - 1;
-    end
-    tints_SLAMS = tints_SLAMS(start_idx:stop_idx);
+    fileID = fopen('test_cell.csv', 'r');
+    fgetl(fileID)
+    tmp = textscan(fileID, '%d %s %f %f %f', 'Delimiter', ',')
+    fclose('all');
+    tmp{:,1}
+    tmp{:,2}
+    tmp{:,3}
+    tmp{:,4}
+    tmp{:,5}
 end
 
